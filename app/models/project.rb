2b8801e4e40984e2e96a16f6dafacfc1f461a3e4
Aaron Duty <aaron@eksdyne.com>
2020-07-16 15:51:27 -0400

    Add a sorted scope to Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
-    has_many :tasks, -> { order(priority: :desc, updated_at: :desc) }, dependent: :destroy
-    has_many :statuses, -> { order(:order) }, dependent: :destroy
+  has_many :tasks, -> { order(order: :asc, priority: :desc, updated_at: :desc) }, dependent: :destroy
+  has_many :statuses, -> { order(:order) }, dependent: :destroy
+
+  scope :sorted, -> { order(:order) }
 end
