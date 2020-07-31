bbe90eb3aac4f9f59c94efb6b6ea324e02e34a66
Aaron Duty <aaron@eksdyne.com>
2020-07-31 16:30:39 -0400

    Actually have the tasks ordered when fetched through the status

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ApplicationRecord
   belongs_to :project
 
-  has_many :tasks, dependent: :destroy
+  has_many :tasks, -> { order(order: :asc, priority: :desc, updated_at: :desc) }, dependent: :destroy
 end
